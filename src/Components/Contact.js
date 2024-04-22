b7ca2bb0da4655808e6d473ec1b060145cd0ef86
vijipriya110 <vijipriya110@gmail.com>
2024-04-22 13:41:14 +0530

    final commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Contact() {
   const config = {
     email:'vijipriya110@gmail.com',
-    phone:'+91 9876543219'
+    phone:'+91 9876543219',
+    link : 'https://drive.google.com/file/d/1gRCdZuQ55qQHhz9mnkL_WDmBr9L_N-8r/view?usp=drive_link'
   }
 
   return (
@@ -12,7 +13,7 @@ function Contact() {
         <div className='flex flex-col items-center'>
          
           <h1 className='text-4xl border-b-4  border-secondary mb-5 w-[130px] font-bold'>Contact</h1>
-          <p className='pb-5'>Looking for a professional web developer to build your next website? Let's talk and contact me today! <a className='bg-secondary px-5 py-2  font-bold hover:border-2 border-white rounded ' href='#'>Download</a></p>
+          <p className='pb-5'>Looking for a professional web developer to build your next website? Let's talk and contact me today! <a className='bg-secondary px-5 py-2  font-bold hover:border-2 border-white rounded ' href={config.link}>Download</a></p>
           <p><span className='font-bold'>Email :</span>{config.email}</p>
           <p><span className='font-bold'>Phone :</span>{config.phone}</p>
          
