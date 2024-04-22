ff236dcd3e71326a4d27649e10292ee81208c05a
vijipriya110 <vijipriya110@gmail.com>
2024-04-22 12:10:58 +0530

    link update

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroImg from '../assets/hero.png'
-import { AiOutlineLinkedin ,AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineLinkedin , AiOutlineGithub } from "react-icons/ai";
 import { CgMail } from "react-icons/cg";
 
 
@@ -8,9 +8,9 @@ function Hero() {
   const config = {
     subtitle:'Expertise in building professional web applications!',
     social : {
-      linkedin:'https://github.com/vijipriya110/bike-rental-front-end',
-      gmail:'https://github.com/vijipriya110/bike-rental-front-end',
-      facebook:'https://github.com/vijipriya110/bike-rental-front-end'
+      linkedin:'https://linkedin.com/in/vijipriya-k',
+      gmail:'https://gmail.com/vijipriya110/',
+      github:'https://github.com/vijipriya110'
        }
   }
   return (
@@ -22,7 +22,7 @@ function Hero() {
     <div className='flex py-10'> 
         <a target='_blank' href={config.social.linkedin} className='pr-5 hover:text-white'><AiOutlineLinkedin size={40}/></a>
         <a target='_blank' href={config.social.gmail} className='pr-5 hover:text-white'><CgMail  size={40}/></a>
-        <a target='_blank' href={config.social.facebook} className='pr-5  hover:text-white'><AiOutlineFacebook size={40}/></a>
+        <a target='_blank' href={config.social.github} className='pr-5  hover:text-white'><AiOutlineGithub size={40}/></a>
 
 
     </div>
